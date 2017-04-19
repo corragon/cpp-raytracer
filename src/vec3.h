c465bc20e171f5d39dec9731b8d4e3d6f5a2ea3c
Thomas Y <corragon@gmail.com>
2017-04-18 23:37:46 -0500

    Add operator overloads for scalar multiplication and division

--- src/vec3.h
+++ src/vec3.h
@@ -46,6 +46,14 @@ inline vec3 operator/(const vec3 &v1, const vec3 &v2) {
   return vec3(v1.e[0] / v2.e[0], v1.e[1] / v2.e[1], v1.e[2] / v2.e[2]);
 }
 
+inline vec3 operator*(const vec3 &v1, const float t) {
+  return vec3(v1.e[0] * t, v1.e[1] * t, v1.e[2] * t);
+}
+
+inline vec3 operator/(const vec3 &v1, const float t) {
+  return vec3(v1.e[0] / t, v1.e[1] / t, v1.e[2] / t);
+}
+
 inline float dot(const vec3 &v1, const vec3 &v2) {
   return v1.e[0] * v2.e[0] + v1.e[1] * v2.e[1] + v1.e[2] * v2.e[2];
 }
